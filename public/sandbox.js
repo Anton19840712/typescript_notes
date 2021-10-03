a4551ec54c744638f52984e8d4a1b761360656d9
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-03 19:24:30 +0300

    Add basic files for ts investigation.

---
+++ public/sandbox.js
@@ -0,0 +1,7 @@
+var element = "antonio banderos"
+console.log(element)
+var inputs = document.querySelectorAll("input")
+console.log(inputs)
+inputs.forEach(function (input) {
+	console.log(input)
+})
