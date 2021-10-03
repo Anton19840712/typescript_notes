1695cb83c463ab56d188b6f8c4171be1535d7d91
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-03 19:25:36 +0300

    Add sample file in ts.

---
+++ source/sandbox.ts
@@ -0,0 +1,11 @@
+const element = "antonio banderos"
+
+console.log(element)
+
+const inputs = document.querySelectorAll("input")
+
+console.log(inputs)
+
+inputs.forEach((input) => {
+	console.log(input)
+})
