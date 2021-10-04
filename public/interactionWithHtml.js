1d1f3abb07bcfccd0269caf11f5ad945a6d744e3
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-04 18:11:14 +0300

    Add sample, how to interact with html.

---
+++ public/interactionWithHtml.js
@@ -0,0 +1,12 @@
+"use strict";
+const form = document.querySelector(".new-item-form");
+//inputs://you can find them as ids inside div in html page.
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+//
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.value);
+});
