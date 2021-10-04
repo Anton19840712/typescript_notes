a2eef10054ed6be76bbf1b2384bce9926111dd2e
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-04 18:14:19 +0300

    Create base sample from ts analog file.

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,8 @@
-var element = "antonio banderos"
-console.log(element)
-var inputs = document.querySelectorAll("input")
-console.log(inputs)
-inputs.forEach(function (input) {
-	console.log(input)
-})
+"use strict";
+const element = "antonio banderos";
+console.log(element);
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach((input) => {
+    console.log(input);
+});
