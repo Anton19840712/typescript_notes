3a8382ee917374c2581f48acd0e8dabc34a4d461
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-05 20:51:50 +0300

    Add new logic.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 //-------------------------------------------
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 let docOne;
 let docTwo;
 docOne = new Invoice("antonio", "web work", 250);
-docOne = new Payment("irina", "plumbing work", 200);
+docTwo = new Payment("irina", "plumbing work", 200);
+let docs = [];
+docs.push(docOne);
 const invOne = new Invoice("mario", "works on the mario website", 250);
 const invTwo = new Invoice("luigi", "works on the luigi website", 300);
 //let invoices: string[] = [] //only strings could be pushed inside this array
@@ -18,11 +21,22 @@ invoices.forEach((inv) => {
 });
 const form = document.querySelector(".new-item-form");
 //inputs//you can find them where id in html
-const type = document.querySelector("#type");
+const type = document.querySelector("#type"); // take a look: we have HTMLSelectElement or HTMLInputElement
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+//list tamplate instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
+    // here we listen to if our button was pushed.
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.value);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end"); //here we ask browser to render a part of UI.
 });
