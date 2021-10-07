6738c657662c2939710de77043d0e4038d3eeb01
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-08 01:39:40 +0300

    Add some sample notes.

---
+++ project_sample/notes.js
@@ -0,0 +1,8 @@
+console.log("Hello, I am Irina");
+let number1 = 5; //you may hover on number1 and see its type actually.
+const person = {
+    name: "Antonio",
+    age: 32,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"],
+};
---
+++ project_sample/notes.ts
@@ -0,0 +1,10 @@
+console.log("Hello, I am Irina")
+
+let number1 = 5 //you may hover on number1 and see its type actually.
+
+const person: object = {
+	name: "Antonio",
+	age: 32,
+	hobbies: ["Sports", "Cooking"],
+	role: [2, "author"],
+}
